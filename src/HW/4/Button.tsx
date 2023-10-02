387b00a0b5d238935fcea09badf6b260a5593f07
matebook <matebook@tech.com>
2023-10-02 23:36:58 +0300

    all homework is done :)

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,16 +1,18 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  callBack: () => void;
+  name: string;
 };
 
 export const Button = (props: ButtonPropsType) => {
+  const { name, callBack } = props;
+
   const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+    callBack();
   };
 
   return (
-    <button id={'hw04-button'} onClick={callBackHandler}>
-      {props.name}
+    <button id={"hw04-button"} onClick={callBackHandler}>
+      {name}
     </button>
   );
 };
--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,23 @@
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
+
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  currentText: string;
+  setCurrentText: Dispatch<SetStateAction<string>>; // () => void
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+  const { currentText, setCurrentText } = props;
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setCurrentText(event.target.value);
+  };
+
+  return (
+    <input
+      id={"hw04-input"}
+      type="text"
+      value={currentText}
+      onChange={onChangeHandler}
+    />
+  );
 };
